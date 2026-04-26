243cb950af154b72ba7cc30917a3a1e086abb171
PurplProjects <ngps27rhxj@privaterelay.appleid.com>
2026-04-26 20:03:12 +0100

    Update SubjectPage.jsx

--- src/pages/SubjectPage.jsx
+++ src/pages/SubjectPage.jsx
@@ -6,6 +6,7 @@ import RevisionPanel from '../components/RevisionPanel'
 
 // Add new subjects here as revision content is built
 const revisionModules = {
+  spanish: () => import('../data/revision/spanish.js').then(m => m.spanishRevision),
   maths:   () => import('../data/revision/maths.js').then(m => m.mathsRevision),
   physics: () => import('../data/revision/physics.js').then(m => m.physicsRevision),
   latin:    () => import('../data/revision/latin.js').then(m => m.latinRevision),
